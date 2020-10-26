de1bd7b282677ed90eab11dd0c538664cfb38c68
Alex <alex.rynkowski90@gmail.com>
2020-10-26 19:29:15 +0100

    adds follow camera

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] GameObject player;
+        
+        void Update()
+        {
+            transform.position = player.transform.position;
+
+        }
+    }
+}
