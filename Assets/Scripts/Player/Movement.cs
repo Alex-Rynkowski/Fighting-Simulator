e3a04132b39b8589d7cabf0c1746974f031a4807
Alex <alex.rynkowski90@gmail.com>
2020-10-26 19:17:07 +0100

    adds simple movement

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] float moveForce = 10;
+
+        Rigidbody _rb;
+
+        void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            ForwardBackwardMovement(KeyCode.W, moveForce);
+            ForwardBackwardMovement(KeyCode.S, -moveForce);
+            LeftRightMovement(KeyCode.D, moveForce);
+            LeftRightMovement(KeyCode.A, -moveForce);
+        }
+
+        void ForwardBackwardMovement(KeyCode userInput, float moveForce)
+        {
+            if (Input.GetKey(userInput))
+            {
+                _rb.AddForce(Vector3.forward * moveForce);
+            }
+        }
+
+        void LeftRightMovement(KeyCode userInput, float moveForce)
+        {
+            if (Input.GetKey(userInput))
+            {
+                _rb.AddForce(Vector3.right * moveForce);
+            }
+        }
+    }
+}
